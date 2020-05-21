04aa76459c10b357bcadd14fc96ffdabf4851d47
pranabendra <30474395+pranabendra@users.noreply.github.com>
2020-05-22 00:02:29 +0530

    Update repository link

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='threephasescim',
-      version='0.8',
+      version='0.9',
 
       packages=find_packages(where='threephasescim'),
       package_dir={'': 'threephasescim'},
       package_data={'threephasescim/lookupTables' : ['*.xlsx']},
       
       description='An obnoxious attempt to design three phase induction motors using Python 3',
-      url='https://bitbucket.org/pranabendra/three-phase-scim-v2/src/master/',
+      url='https://github.com/pranabendra/three-phase-scim',
       author='Pranabendra Prasad Chandra',
       author_email='pranabendrachandra@gmail.com',
       license='MIT',
