e5f5d967d08b22962dc6ed9af219b075df830efc
pranabendra <30474395+pranabendra@users.noreply.github.com>
2020-05-22 17:07:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+      long_description = f.read()
 
 setup(name='threephasescim',
-      version='0.9.4',
+      version='0.9.5',
 
       packages=find_packages(where='threephasescim'),
       package_dir={'': 'threephasescim'},
@@ -13,5 +18,7 @@ setup(name='threephasescim',
       author_email='pranabendrachandra@gmail.com',
       license='MIT',
       include_package_data=True,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       install_requires=['pandas', 'numpy', 'scipy', 'xlrd', 'openpyxl',],
       zip_safe=False)
