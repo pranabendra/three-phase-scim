6f48345a48ee036283e737c8db320cdf8977e6f6
pranabendra <30474395+pranabendra@users.noreply.github.com>
2020-05-22 18:12:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
       long_description = f.read()
 
+CLASSIFIERS = [
+    'Intended Audience :: Education',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+]
+
 setup(name='threephasescim',
       version='0.1.0',
 
@@ -17,6 +23,7 @@ setup(name='threephasescim',
       author='Pranabendra Prasad Chandra',
       author_email='pranabendrachandra@gmail.com',
       license='MIT',
+      classifiers=CLASSIFIERS,
       include_package_data=True,
       long_description=long_description,
       long_description_content_type='text/markdown',
